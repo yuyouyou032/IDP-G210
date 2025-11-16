11245ff2137425b980f371f72b6e2a2bdb086b3c
Mengjie Lu <ml2187@cam.ac.uk>
2025-11-16 22:30:05 +0000

    Added support for sensor to handle left/right junctions

--- control/main.py
+++ control/main.py
@@ -13,47 +13,59 @@ from test_tcs3472 import test_tcs3472
 # from test_tiny_code_reader import test_tiny_code_reader
 
 print("Welcome to main.py!")
-pin_sensor1 = 12
+pin_sensor1 = 12  # middle-right
 pin_sensor2 = 13
+# New additional sensors for turning left/right
+pin_sensor3 = 7      # right-most
+pin_sensor4 = 8
 #
-last_readings=sensor12(12,13)
+last_readings=sensor12(pin_sensor1, pin_sensor2)
 dev_sensors_readings = [1,1]
 while True:
-    dev_sensors_readings = sensor12(12,13) #     pin 16, 17
-    print(dev_sensors_readings)
+    dev_sensors_readings = sensor12(pin_sensor1, pin_sensor2)
+    print("2 front sensors:", dev_sensors_readings)
+
+    MR, ML, FR, FL = sensor1234(pin_sensor1, pin_sensor2, pin_sensor3, pin_sensor4)
+    # FL=far-left, ML=mid-left, MR=mid-right, FR=far-right
+    print("4 sensors:", [FL, ML, MR, FR])
+
+    if FL == 1:
+        print("LEFT junction detected -> CCW turn")
+        CCW(40, 0.25)
+        continue
+
+    if FR == 0:
+        print("RIGHT junction detected -> CW turn")
+        CW(40, 0.25)
+        continue
+
     if dev_sensors_readings[0] == 0 and dev_sensors_readings[1] == 1:
         CCW(30, 0.2)
-        last_readings=dev_sensors_readings
-        print("CW")
+        last_readings = dev_sensors_readings
+        print("CCW")
+
     if dev_sensors_readings[1] == 0 and dev_sensors_readings[0] == 1:
         CW(30, 0.2)
-        last_readings=dev_sensors_readings
-        print("CCW")
-    
+        last_readings = dev_sensors_readings
+        print("CW")
+
     if dev_sensors_readings[1] == 1 and dev_sensors_readings[0] == 1:
         wheels_forward(30, 0.1)
         print("forward")
-    
 
-        
     counter = 0
-    while dev_sensors_readings[0]== 0 and dev_sensors_readings[1]==0:
+    while dev_sensors_readings[0] == 0 and dev_sensors_readings[1] == 0:
         print(counter)
         counter += 1
-        if last_readings[0]==1:
-            print("finding rounte - cw")
-            CW(40,0.1)
-        if last_readings[1]==1:
-            print("finding rounte - ccw")
-            CCW(40,0.1)
-        dev_sensors_readings = sensor12(12,13)
+        if last_readings[0] == 1:
+            print("Left off - cw")
+            CW(40, 0.1)
+        if last_readings[1] == 1:
+            print("Right off - ccw")
+            CCW(40, 0.1)
+        dev_sensors_readings = sensor12(pin_sensor1, pin_sensor2)
         print(dev_sensors_readings)
-        
-    
-
-    
             
-    
     
 #     turn_sensor_readings = sensor12(14, 15)
 
@@ -80,5 +92,6 @@ turn_off()
 # test_tiny_code_reader()
 
 print("main.py Done!")
+
 
 
